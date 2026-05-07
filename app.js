5b96e08599b732d3b71ab53b9358265209a18226
Mark Fruman <majorgowan@yahoo.com>
2026-05-06 22:33:07 -0400

    adding mongo query and EJS template to display weekly stats report

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require('express');
-const { connectToDatabase } = require("./db/db");
+const {connectToDatabase} = require("./db/db");
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -53,17 +53,148 @@ app.get('/data', (req, res) => {
 });
 
 app.get('/exJays', (req, res) => {
-    // serve filtered or unfiltered data
     const players = Object.values(filterPlayers("ex"));
     console.log(`Returning ${players.length} ex Blue Jays.`);
-    res.render('report', { players: players });
+    res.render('exjays', {players: players});
+});
+
+app.get('/report', async (req, res) => {
+    let endDate = req.query.endDate;
+    const dbInstance = await connectToDatabase("exbluejays");
+    // if no date specified, get the latest stats from Mongo
+    if (endDate === undefined) {
+        const lastDate = await dbInstance.collection("players").aggregate([
+            {
+                "$match": {
+                    "stats": {
+                        "$exists": true
+                    }
+                }
+            },
+            {
+                "$project": {
+                    "keys": {
+                        "$objectToArray": "$stats"
+                    }
+                }
+            },
+            {
+                "$unwind": "$keys"
+            },
+            {
+                "$group": {
+                    "_id": null,
+                    "lastDate": {
+                        "$max": "$keys.k"
+                    }
+                }
+            }
+        ]).toArray();
+        endDate = lastDate[0].lastDate;
+    }
+    // retrieve stats for specified date
+    const playersArray = await dbInstance.collection("players").aggregate([
+        {
+            "$match": {
+                [`stats.${endDate}`]: {"$exists": true}
+            }
+        },
+        {
+            "$project": {
+                "_id": 1,
+                "fullName": 1,
+                "position": 1,
+                "latest_team": 1,
+                [`stats.${endDate}`]: 1,
+                "years_with_jays": 1
+            }
+        },
+        {
+            "$sort": {
+                [`stats.${endDate}.ops`]: -1
+            }
+        }
+    ]).toArray();
+    // build hitters table
+    const hitters = await playersArray
+        .filter(player => {
+            return (player.position !== "Pitcher");
+        }).map(player => {
+            return {
+                "_id": player._id,
+                "name": player.fullName,
+                "position": player.position,
+                "team": player.latest_team,
+                "ex_since": player.years_with_jays.at(-1),
+                "gamesPlayed": player.stats[endDate].gamesPlayed,
+                "atBats": player.stats[endDate].atBats,
+                "avg": player.stats[endDate].avg,
+                "obp": player.stats[endDate].obp,
+                "slg": player.stats[endDate].slg,
+                "ops": player.stats[endDate].ops,
+                "hits": player.stats[endDate].hits,
+                "runs": player.stats[endDate].runs,
+                "homeRuns": player.stats[endDate].homeRuns,
+                "rbi": player.stats[endDate].rbi,
+                "strikeOuts": player.stats[endDate].strikeOuts,
+                "baseOnBalls": player.stats[endDate].baseOnBalls,
+                "stolenBases": player.stats[endDate].stolenBases,
+                "groundIntoDoublePlay": player.stats[endDate].groundIntoDoublePlay,
+                "plateAppearances": player.stats[endDate].plateAppearances
+            };
+        });
+
+    // build pitchers table
+    const pitchers = await playersArray
+        .filter(player => {
+            return (player.position === "Pitcher");
+        }).map(player => {
+            return {
+                "_id": player._id,
+                "name": player.fullName,
+                "team": player.latest_team,
+                "ex_since": player.years_with_jays.at(-1),
+                "gamesPitched": player.stats[endDate].gamesPitched,
+                "gamesStarted": player.stats[endDate].gamesStarted,
+                "inningsPitched": player.stats[endDate].inningsPitched,
+                "wins": player.stats[endDate].wins,
+                "losses": player.stats[endDate].losses,
+                "era": player.stats[endDate].era,
+                "whip": player.stats[endDate].whip,
+                "saves": player.stats[endDate].saves,
+                "holds": player.stats[endDate].holds,
+                "hits": player.stats[endDate].hits,
+                "homeRuns": player.stats[endDate].homeRuns,
+                "earnedRuns": player.stats[endDate].earnedRuns,
+                "strikeOuts": player.stats[endDate].strikeOuts,
+                "baseOnBalls": player.stats[endDate].baseOnBalls
+            };
+        });
+    // sort pitchers by ERA then WHIP
+    pitchers.sort((pitcherA, pitcherB) => {
+        const diff = pitcherA.era - pitcherB.era;
+        if (diff === 0) {
+            return pitcherA.whip - pitcherB.whip;
+        }
+        return diff;
+    });
+
+    const endDateString = new Date(endDate).toLocaleDateString('en-US',
+        {weekday: "long", month: "long", day: "numeric"}
+    );
+
+    console.log(`Returning ${hitters.length} ex Blue Jay hitters and ${pitchers.length} ex Blue Jay pitchers.`);
+    res.render('report',
+        {
+            endDate: endDateString,
+            hitters: hitters,
+            pitchers: pitchers
+        });
 });
 
 app.get('/db', async (req, res) => {
     const dbInstance = await connectToDatabase("exbluejays");
-    console.log(dbInstance);
     const playersArray = await dbInstance.collection("players").find({}).toArray();
-    console.log(playersArray);
     res.json(playersArray);
 });
 
@@ -76,9 +207,11 @@ app.post('/populate', async (req, res) => {
     });
     const playersCollection = dbInstance.collection("players");
     const result = await playersCollection.insertMany(documentsToInsert,
-                                                                       {"ordered": false});
-    res.json({"inserted": result.insertedCount,
-                   "totalDocs": await playersCollection.countDocuments()});
+        {"ordered": false});
+    res.json({
+        "inserted": result.insertedCount,
+        "totalDocs": await playersCollection.countDocuments()
+    });
 });
 
 app.listen(3000, () => {
