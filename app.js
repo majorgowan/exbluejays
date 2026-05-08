c4a29a2222d0c84a4a63a79ed72423eec9bb78e7
Mark Fruman <majorgowan@yahoo.com>
2026-05-08 17:22:48 -0400

    commenting out references to local data files

--- app.js
+++ app.js
@@ -10,30 +10,30 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 
 // (temporary) local file with player data
-const playersData = require('./data/players_with_activity.json');
+// const playersData = require('./data/players_with_activity.json');
 
 const currentYear = new Date().getFullYear().toString();
 
 // function to filter players
-const filterPlayers = (how) => {
-    if (how === "current") {
-        // get current Jays
-        return Object.fromEntries(
-            Object.entries(playersData).filter(([id, player]) => {
-                return (player.active
-                    && player.years_with_jays.includes(currentYear)
-                    && player.latest_team.includes("Toronto"));
-            }));
-    } else if (how === "ex") {
-        // get active former Jays
-        return Object.fromEntries(
-            Object.entries(playersData).filter(([id, player]) => {
-                return (player.active
-                    && player.years_with_jays.length > 0
-                    && !player.latest_team.includes("Toronto"));
-            }));
-    }
-};
+// const filterPlayers = (how) => {
+//     if (how === "current") {
+//         // get current Jays
+//         return Object.fromEntries(
+//             Object.entries(playersData).filter(([id, player]) => {
+//                 return (player.active
+//                     && player.years_with_jays.includes(currentYear)
+//                     && player.latest_team.includes("Toronto"));
+//             }));
+//     } else if (how === "ex") {
+//         // get active former Jays
+//         return Object.fromEntries(
+//             Object.entries(playersData).filter(([id, player]) => {
+//                 return (player.active
+//                     && player.years_with_jays.length > 0
+//                     && !player.latest_team.includes("Toronto"));
+//             }));
+//     }
+// };
 
 app.use(express.static("public"));
 
@@ -41,25 +41,25 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.get('/data', (req, res) => {
-    // serve filtered or unfiltered data
-    const allOrSome = req.query.all;
-    console.log(allOrSome);
-    if (allOrSome === "all") {
-        res.json(playersData);
-    } else {
-        // current or ex Blue Jays
-        const filteredList = filterPlayers(allOrSome);
-        console.log(`Returning ${Object.entries(filteredList).length} ${allOrSome} Blue Jays.`);
-        res.json(filteredList);
-    }
-});
+// app.get('/data', (req, res) => {
+//     // serve filtered or unfiltered data
+//     const allOrSome = req.query.all;
+//     console.log(allOrSome);
+//     if (allOrSome === "all") {
+//         res.json(playersData);
+//     } else {
+//         // current or ex Blue Jays
+//         const filteredList = filterPlayers(allOrSome);
+//         console.log(`Returning ${Object.entries(filteredList).length} ${allOrSome} Blue Jays.`);
+//         res.json(filteredList);
+//     }
+// });
 
-app.get('/roster', (req, res) => {
-    const players = Object.values(filterPlayers("ex"));
-    console.log(`Returning ${players.length} ex Blue Jays.`);
-    res.render('roster', {players: players});
-});
+// app.get('/roster', (req, res) => {
+//     const players = Object.values(filterPlayers("ex"));
+//     console.log(`Returning ${players.length} ex Blue Jays.`);
+//     res.render('roster', {players: players});
+// });
 
 app.get('/report', async (req, res) => {
     let endDate = req.query.endDate;
@@ -200,21 +200,21 @@ app.get('/db', async (req, res) => {
     res.json(playersArray);
 });
 
-app.post('/populate', async (req, res) => {
-    const dbInstance = await connectToDatabase("exbluejays");
-    const how = req.body.how;
-    const players = filterPlayers(how);
-    const documentsToInsert = Object.entries(players).map(([id, player]) => {
-        return {"_id": id, ...player};
-    });
-    const playersCollection = dbInstance.collection("players");
-    const result = await playersCollection.insertMany(documentsToInsert,
-        {"ordered": false});
-    res.json({
-        "inserted": result.insertedCount,
-        "totalDocs": await playersCollection.countDocuments()
-    });
-});
+// app.post('/populate', async (req, res) => {
+//     const dbInstance = await connectToDatabase("exbluejays");
+//     const how = req.body.how;
+//     const players = filterPlayers(how);
+//     const documentsToInsert = Object.entries(players).map(([id, player]) => {
+//         return {"_id": id, ...player};
+//     });
+//     const playersCollection = dbInstance.collection("players");
+//     const result = await playersCollection.insertMany(documentsToInsert,
+//         {"ordered": false});
+//     res.json({
+//         "inserted": result.insertedCount,
+//         "totalDocs": await playersCollection.countDocuments()
+//     });
+// });
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server running on port 3000");
