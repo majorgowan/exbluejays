1d2f7f3107eb55e722e608450c5639d04d2e96ac
Mark Fruman <majorgowan@yahoo.com>
2026-05-19 10:28:18 -0400

    make_summary working great

--- scripts/make_summary.js
+++ scripts/make_summary.js
@@ -8,7 +8,21 @@ const argv = require('yargs')
     .option("endDate", {
         type: "string",
         describe: "specify end date"
+    })
+    .option("testing", {
+        type: "boolean",
+        default: false,
+        describe: "do not update mongo"
+    })
+    .option("verbose", {
+        alias: "v",
+        type: "boolean",
+        default: false,
+        describe: "generate verbose output to console"
     }).argv;
+
+const verbose = argv.verbose;
+const testing = argv.testing;
 
 let endDate = argv.endDate;
 if (endDate === undefined) {
@@ -17,42 +31,74 @@ if (endDate === undefined) {
 
 async function makeSummary() {
 
-    // const dbInstance = await connectToDatabase("exbluejays");
-    //
-    // // get stats from Mongo
-    // const { hitters: hitters_week, pitchers: pitchers_week } = await buildTables(dbInstance, "stats", endDate);
-    // const { hitters: hitters_ytd, pitchers: pitchers_ytd } = await buildTables(dbInstance, "ytd", endDate);
-    //
-    // // only list the best few hitters and pitchers
-    // // sort pitchers by Runs Prevented
-    // pitchers_week.sort((pa, pb) => {
-    //     return pb.frumans - pa.frumans;
-    // });
-    // pitchers_ytd.sort((pa, pb) => {
-    //    return pb.frumans - pa.frumans;
-    // });
-    //
-    // // sort hitters by Runs Created
-    // hitters_week.sort((ha, hb) => {
-    //     return hb.runsCreated - ha.runsCreated;
-    // });
-    // hitters_ytd.sort((ha, hb) => {
-    //     return hb.runsCreated - ha.runsCreated;
-    // });
-    //
-    // hitters_week.length = 8;
-    // pitchers_week.length = 8;
-    // hitters_ytd.length = 8;
-    // pitchers_ytd.length = 8;
-    //
-    // // get schedule for the week to come
-    // const schedule = await buildSeries(dbInstance, endDate);
-    //
+    const dbInstance = await connectToDatabase("exbluejays");
 
-    // await closeConnection();
-    const prompt = buildPrompt();
+    // get stats from Mongo
+    const { hitters: hitters_week, pitchers: pitchers_week } = await buildTables(dbInstance, "stats", endDate);
+    const { hitters: hitters_ytd, pitchers: pitchers_ytd } = await buildTables(dbInstance, "ytd", endDate);
+
+    // only list the best few hitters and pitchers
+    // sort pitchers by Runs Prevented
+    pitchers_week.sort((pa, pb) => {
+        return pb.frumans - pa.frumans;
+    });
+    pitchers_ytd.sort((pa, pb) => {
+       return pb.frumans - pa.frumans;
+    });
+
+    // sort hitters by Runs Created
+    hitters_week.sort((ha, hb) => {
+        return hb.runsCreated - ha.runsCreated;
+    });
+    hitters_ytd.sort((ha, hb) => {
+        return hb.runsCreated - ha.runsCreated;
+    });
+
+    hitters_week.length = 8;
+    pitchers_week.length = 8;
+    hitters_ytd.length = 8;
+    pitchers_ytd.length = 8;
+
+    // get schedule for the week to come
+    const schedule = await buildSeries(dbInstance, endDate);
+
+    const prompt = buildPrompt(hitters_week, hitters_ytd, pitchers_week, pitchers_ytd,
+                                     schedule);
+    if (verbose) {
+        console.log("Prompt: \n");
+        console.log(prompt);
+        console.log("\n\n");
+    }
+
     const summary = await askCerebras(prompt);
-    console.log(summary);
+    if (verbose) {
+        console.log("summary: \n\n");
+        console.log(summary);
+    }
+
+    if (!testing) {
+        // update reports collection
+        const reportsCollection = dbInstance.collection("reports");
+        const reportsResult = await reportsCollection.updateOne(
+            {
+                "endDate": endDate
+            },
+            {
+                "$set":
+                    {
+                        "summary": summary
+                    }
+            },
+            {
+                "upsert": true
+            }
+        );
+        if (reportsResult.acknowledged) {
+            if (verbose) console.log(reportsResult);
+        }
+    }
+
+    await closeConnection();
 }
 
 makeSummary();
--- utils/cerebras.js
+++ utils/cerebras.js
@@ -1,4 +1,5 @@
 const Cerebras = require("@cerebras/cerebras_cloud_sdk");
+const jsonToMarkdown = require("json-to-markdown-table");
 
 const client = new Cerebras({
     apiKey: process.env["CEREBRAS_API_KEY"],
@@ -31,9 +32,52 @@ async function askCerebras(content, max_completion_tokens=1024, temperature=0.2)
     }
 }
 
-function buildPrompt(hitters=null, pitchers=null, notes=null, news=null) {
+function buildPrompt(hitters_week, hitters_ytd, pitchers_week, pitchers_ytd,
+                     schedule, notes=null, news=null) {
     // generate the content for asking cerebras
-    const prompt = "Show me the money!!!!";
+
+    const scheduleString = Object.values(schedule).map(row => {
+        return (`\n- ${row.phrase} ${row.opponent} at ${row.venue} from ${row.from_date} to ${row.to_date}`
+                + " where they might face "
+                + (row.exArms.length > 0 ? `ex arms ${row.exArms.join(",")}` : "")
+                + (row.exBats.length > 0 ? `ex bats ${row.exBats.join(",")}` : ""));
+    });
+
+    let prompt = `Your name is Mister Ex and you are an old timey sports reporter whose beat is former Toronto Blue Jays.
+    
+    Here are statistics for some former Toronto Blue Jays players for the last week.  The
+    column "ex_since" represents the last year they played with Toronto.
+    
+    Hitters:
+    ${jsonToMarkdown(hitters_week, Object.keys(hitters_week[0]))}
+    
+    Pitchers:
+    ${jsonToMarkdown(pitchers_week, Object.keys(pitchers_week[0]))}
+    
+    and here are some stats for the season up to now:
+    
+    Hitters:
+    ${jsonToMarkdown(hitters_ytd, Object.keys(hitters_ytd[0]))}
+    
+    Pitchers:
+    ${jsonToMarkdown(pitchers_ytd, Object.keys(pitchers_ytd[0]))}
+    
+    The Blue Jays' schedule for the next week is: 
+    ${scheduleString.join("\n")}
+    
+    In three or four sentences, please summarize the outstanding performances from the past week?
+    
+    In a sentence or two, mention the best hitters and pitchers of the season so far.
+    
+    In a final sentence, mention the Blue Jays' opponents for the next week and any former teammates they might
+    face in those series. 
+    
+    Please use the style of a 1950s sports report.  Please do not accidentally say any of the players are still with the Blue Jays.
+    
+    Introduce yourself at the beginning.  
+    
+    Try to work in the year the players last played for Toronto and how they are fondly remembered.`
+
     return prompt
 }
 
